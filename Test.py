eecbeb35812e8d6e425338be1ed3c6d08091db53
pajnn <cesar.arteagabarrera@gmail.com>
2024-08-23 12:27:29 -0500

    First commit

---
+++ Test.py
@@ -0,0 +1,20 @@
+first_letter = '''
+ ccc
+c   c
+c
+c
+c
+c   c
+ ccc'''
+print (first_letter)
+
+second_letter = '''
+  a
+ a  a
+a    a
+a a a a
+a     a
+a     a
+a     a'''
+
+print (second_letter)
